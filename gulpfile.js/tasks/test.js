dc03852d160aeff517f107e8d37618300b1edbc1
Charles Korn <me@charleskorn.com>
2015-10-24 17:25:28 +1100

    Add a task to watch for file modifications and automatically re-run the tests.

--- gulpfile.js/tasks/test.js
+++ gulpfile.js/tasks/test.js
@@ -1,6 +1,10 @@
+"use strict";
+
 var gulp = require("gulp");
 var shell = require("gulp-shell");
 var path = require("path");
+var watch = require("gulp-watch");
+var batch = require("gulp-batch");
 
 var config = require("../config");
 
@@ -9,3 +13,14 @@ var testBinary = path.resolve(config.build.output.tests + "/bin/test_runner");
 gulp.task("test", ["build:tests"],
   shell.task(testBinary + " --force-colour")
 );
+
+gulp.task("test:watch", ["test"], function () {
+  var pathsToWatch = [
+    config.build.sources.application + "/**/*",
+    config.build.sources.tests + "/**/*"
+  ];
+
+  watch(pathsToWatch, batch(function (events, done) {
+      gulp.start("test", done);
+  }));
+});
