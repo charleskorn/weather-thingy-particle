1a677c11f72ba7d5d4610e78df656a019b868a63
Charles Korn <me@charleskorn.com>
2015-12-26 21:15:56 +1100

    Added reporting illuminance value.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "application.h"
 #include "rht03.hpp"
+#include "temt6000.hpp"
 
 #define TEMP_SENSOR_PIN D6
+#define LIGHT_SENSOR_PIN A1
 
-void readSensorData();
+RHT03Sensor temperatureSensor(TEMP_SENSOR_PIN);
+TEMT6000Sensor lightSensor(LIGHT_SENSOR_PIN);
+
+void readTemperatureSensorData();
+void readLightSensorData();
 
 // cppcheck-suppress unusedFunction Used by Particle framework.
 void setup() {
@@ -12,17 +18,31 @@ void setup() {
 
 // cppcheck-suppress unusedFunction Used by Particle framework.
 void loop() {
-  readSensorData();
+  readTemperatureSensorData();
+  readLightSensorData();
 
   delay(10000);
 }
 
-void readSensorData() {
-  RHT03Sensor sensor(TEMP_SENSOR_PIN);
-  RHT03SensorData result = sensor.readSensorData();
+void readTemperatureSensorData() {
+  RHT03SensorData result = temperatureSensor.readSensorData();
 
-  Particle.publish("Timed out?", String(result.timedOut));
-  Particle.publish("Checksum OK?", String(result.checksumOK));
-  Particle.publish("Humidity", String(result.humidity));
-  Particle.publish("Temperature", String(result.temperature));
+  // We're not allowed to publish more than 1 event a second
+  // (or burst beyond four events in one second with three seconds to recover)
+  // See https://docs.particle.io/reference/firmware/photon/#particle-publish-
+  if (result.timedOut) {
+    Particle.publish("Temperature sensor: status", "Timed out");
+  } else if (!result.checksumOK) {
+    Particle.publish("Temperature sensor: status", "Checksum failed");
+  } else {
+    Particle.publish("Temperature sensor: status", "OK");
+    Particle.publish("Temperature sensor: humidity", String(result.humidity));
+    Particle.publish("Temperature sensor: temperature", String(result.temperature));
+  }
+}
+
+void readLightSensorData() {
+  float illuminance = lightSensor.readIlluminance();
+
+  Particle.publish("Light sensor: illuminance", String(illuminance));
 }
