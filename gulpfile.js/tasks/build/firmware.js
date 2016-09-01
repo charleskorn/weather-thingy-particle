f6dc43edd71a0b72fc98734bd8eadeb36fd592a6
Charles Korn <me@charleskorn.com>
2016-09-01 15:37:50 +1000

    Removed non-functional flash-local task. Reworked build task to use cloud compilation.

--- gulpfile.js/tasks/build/firmware.js
+++ gulpfile.js/tasks/build/firmware.js
@@ -3,9 +3,14 @@ var shell = require("gulp-shell");
 
 var config = require("../../config");
 var particleHelpers = require("../../particleHelpers");
+var targetPath = config.particle.targetName + ".bin";
 
 gulp.task("build:firmware",
-  shell.task(["make all" + particleHelpers.commonMakeVariables], {
-    cwd: config.particle.firmwareDirectory + "/main"
+  shell.task([
+  	"mkdir -p " + config.build.output.firmware,
+  	"cd " + config.build.output.firmware + " && rm -f *.bin && particle compile " + config.particle.platform + " " + config.build.sources.application + "/* && " +
+  	"echo 'Renaming binary to " + targetPath + "' && mv *.bin " + targetPath
+  	], {
+    cwd: config.build.output.baseDirectory
   })
 );
